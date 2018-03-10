f297ac3244072c87546b25f324857203971d1ab4
ssborbis <ssborbis@gmail.com>
2018-03-09 18:46:24 -0600

    Delete update.js

--- update.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('options_a').addEventListener('click',openOptions);
-		
-function openOptions() {
-	browser.runtime.sendMessage({action: "openOptions"});
-}
-
-document.getElementById('version').innerText = browser.runtime.getManifest().version;
