9a5523485a53097e3ae7b903bbb3f60f00e3be8e
ssborbis <ssborbis@gmail.com>
2019-08-11 04:03:25 -0500

    repeat search fix

--- src/tools.js
+++ src/tools.js
@@ -4,13 +4,13 @@ var QMtools = [
 		icon: "icons/close.png", 
 		title: browser.i18n.getMessage('tools_Close'),
 		init: function() {
-			let tile_close = buildSearchIcon(browser.runtime.getURL(this.icon), this.title);
+			let tile = buildSearchIcon(browser.runtime.getURL(this.icon), this.title);
 
-			addTileEventHandlers(tile_close, (e) => {
+			addTileEventHandlers(tile, (e) => {
 				browser.runtime.sendMessage({action: "closeQuickMenuRequest", eventType: "click_close_icon"});
 			});
 			
-			return tile_close;
+			return tile;
 		}
 	},
 	{
@@ -18,9 +18,9 @@ var QMtools = [
 		icon: "icons/clipboard.png", 
 		title: browser.i18n.getMessage('tools_Copy'),
 		init: function() {
-			let tile_copy = buildSearchIcon(browser.runtime.getURL(this.icon), this.title);
+			let tile = buildSearchIcon(browser.runtime.getURL(this.icon), this.title);
 					
-			addTileEventHandlers(tile_copy, (e) => {
+			addTileEventHandlers(tile, (e) => {
 
 				let input = document.createElement('input');
 				input.type = "text";
@@ -38,9 +38,14 @@ var QMtools = [
 				
 				// chrome requires execCommand be run from background
 				browser.runtime.sendMessage({action: 'copy', msg: getSearchBar().value});
+				
+				tile.style.backgroundImage = `url(${browser.runtime.getURL('icons/checkmark.svg')})`;
+				setTimeout( () => {
+					tile.style.backgroundImage = `url(${browser.runtime.getURL(this.icon)})`;
+				}, 500);
 			});
 			
-			return tile_copy;
+			return tile;
 		}
 	},
 	{
@@ -48,14 +53,14 @@ var QMtools = [
 		icon: "icons/link.svg", 
 		title: browser.i18n.getMessage('tools_OpenAsLink'),
 		init: function() {
-			let tile_link = buildSearchIcon(browser.runtime.getURL(this.icon), this.title);
+			let tile = buildSearchIcon(browser.runtime.getURL(this.icon), this.title);
 
 			// enable/disable link button on very basic 'is it a link' rules
 			function setDisabled() {
 				if (quickMenuObject.searchTerms.trim().indexOf(" ") !== -1 || quickMenuObject.searchTerms.indexOf(".") === -1) {
-					tile_link.dataset.disabled = true;
+					tile.dataset.disabled = true;
 				} else {
-					tile_link.dataset.disabled = false;
+					tile.dataset.disabled = false;
 				}
 			}
 			
@@ -67,9 +72,9 @@ var QMtools = [
 				setDisabled();
 			});
 			
-			addTileEventHandlers(tile_link, (e) => {
+			addTileEventHandlers(tile, (e) => {
 
-				if (tile_link.dataset.disabled === "true") return;
+				if (tile.dataset.disabled === "true") return;
 
 				browser.runtime.sendMessage({
 					action: "quickMenuSearch", 
@@ -82,7 +87,7 @@ var QMtools = [
 				});
 			});
 			
-			return tile_link;
+			return tile;
 		}
 	},
 	{
@@ -90,8 +95,8 @@ var QMtools = [
 		icon: "icons/power.svg", 
 		title: browser.i18n.getMessage('tools_Disable'),
 		init: function() {
-			let tile_disable = buildSearchIcon(browser.runtime.getURL(this.icon), this.title);
-			addTileEventHandlers(tile_disable, (e) => {
+			let tile = buildSearchIcon(browser.runtime.getURL(this.icon), this.title);
+			addTileEventHandlers(tile, (e) => {
 				
 				userOptions.quickMenu = false;
 				quickMenuObject.disabled = true;
@@ -106,7 +111,7 @@ var QMtools = [
 				browser.runtime.sendMessage({action: "closeQuickMenuRequest", eventType: "click_disable_icon"});
 			});
 			
-			return tile_disable;
+			return tile;
 		}
 	},
 	{
@@ -114,10 +119,10 @@ var QMtools = [
 		icon: "icons/lock.png", 
 		title: browser.i18n.getMessage('tools_Lock'),
 		init: function() {
-			let tile_lock = buildSearchIcon(browser.runtime.getURL(this.icon), this.title);
+			let tile = buildSearchIcon(browser.runtime.getURL(this.icon), this.title);
 					
-			tile_lock.dataset.locked = false;
-			addTileEventHandlers(tile_lock, (e) => {
+			tile.dataset.locked = false;
+			addTileEventHandlers(tile, (e) => {
 
 				if ( this.dataset.locked === "true" )
 					this.dataset.locked = quickMenuObject.locked = false;
@@ -132,7 +137,7 @@ var QMtools = [
 				});
 			});
 			
-			return tile_lock;
+			return tile;
 		}
 	},
 	{
@@ -141,7 +146,7 @@ var QMtools = [
 		title: browser.i18n.getMessage('tools_lastused'),
 		init: function() {
 
-			let tile_lastused = buildSearchIcon(browser.runtime.getURL(this.icon), this.title);
+			let tile = buildSearchIcon(browser.runtime.getURL(this.icon), this.title);
 			
 			function updateIcon() {
 
@@ -150,6 +155,8 @@ var QMtools = [
 					let _id = result.shift().lastUsed;
 
 					if ( _id ) {
+						
+						tile.dataset.disabled = false;
 				
 						let node = findNodes(userOptions.nodeTree, _node => _node.id === _id)[0];
 							
@@ -163,9 +170,10 @@ var QMtools = [
 							}
 						}() || browser.runtime.getURL('icons/search.svg');
 						
-						tile_lastused.style.backgroundImage = 'url(' + icon + ')';
-						tile_lastused.title = node.title;
-					}
+						tile.style.backgroundImage = `url(${icon})`;
+						tile.title = node.title;
+					} else
+						tile.dataset.disabled = true;
 				});
 			}
 			
@@ -173,9 +181,11 @@ var QMtools = [
 
 			document.addEventListener('updatesearchterms', updateIcon); // fires when a search executes, piggybacking for icon update
 
-			addTileEventHandlers(tile_lastused, (e) => {
+			addTileEventHandlers(tile, (e) => {
 			
-				let lastUsedId = quickMenuObject.lastUsed || quickMenuElement.querySelector('[data-type="searchEngine"]').node.id || null;
+				let lastUsedId = quickMenuObject.lastUsed || null;// || quickMenuElement.querySelector('[data-type="searchEngine"]').node.id || null;
+				
+				if ( !lastUsedId) return;
 
 				quickMenuObject.lastUsed = lastUsedId;
 				browser.runtime.sendMessage({
@@ -188,7 +198,7 @@ var QMtools = [
 				});
 			});
 			
-			return tile_lastused;
+			return tile;
 		}
 	},
 	{
@@ -196,21 +206,21 @@ var QMtools = [
 		icon: "icons/repeatsearch.svg", 
 		title: browser.i18n.getMessage('tools_repeatsearch'),
 		init: function() {
-			let tile_qs = buildSearchIcon(browser.runtime.getURL(this.icon), this.title);
+			let tile = buildSearchIcon(browser.runtime.getURL(this.icon), this.title);
 
 			browser.runtime.sendMessage({action: "getTabQuickMenuObject"}).then( result => {
-				tile_qs.dataset.disabled = tile_qs.disabled = !result.shift().repeatsearch;
+				tile.dataset.disabled = !result.shift().repeatsearch;
 			});
 
-			addTileEventHandlers(tile_qs, (e) => {
+			addTileEventHandlers(tile, (e) => {
 
 				let lastUsedId = quickMenuObject.lastUsed || quickMenuElement.querySelector('[data-type="searchEngine"]').node.id || null;
 				
 				quickMenuObject.lastUsed = lastUsedId;
 				
-				tile_qs.disabled = tile_qs.dataset.disabled = !tile_qs.disabled;
+				quickMenuObject.repeatsearch = (tile.dataset.disabled == "true") ? true : false;
 				
-				quickMenuObject.repeatsearch = !tile_qs.disabled;
+				tile.dataset.disabled = !quickMenuObject.repeatsearch;
 
 				browser.runtime.sendMessage({
 					action: "updateQuickMenuObject", 
@@ -218,7 +228,7 @@ var QMtools = [
 				});
 			});
 			
-			return tile_qs;
+			return tile;
 		}
 	}
 ];
