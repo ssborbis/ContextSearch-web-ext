8083e17bdd129630ca14e66ca3c4c67ff2e15ca9
ssborbis <ssborbis@gmail.com>
2021-02-11 15:39:00 -0600

    icon cleanup page tiles cancles on mouse shake ( FF only )

--- src/pagetiles.js
+++ src/pagetiles.js
@@ -5,6 +5,7 @@ browser.runtime.sendMessage({action: "getUserOptions"}).then( message => {
 
 	setUserStyles();
 	makePageTiles();
+	if ( true ) detectShake();
 })
 
 // document.addEventListener('mouseup', e => {
@@ -148,4 +149,49 @@ function setUserStyles() {
 		
 		document.body.getBoundingClientRect();
 	}
+}
+
+let detectShake = function() {
+
+	let lastMovementX = 0;
+	let reversals = [];
+	const shake_threshold = 5;
+
+	// function handler(event) {
+	// 	if ( event.movementX * lastMovementX < 0 )
+	// 		reversals.push(Date.now());
+
+	// 	if ( reversals.length > shake_threshold ) reversals.shift();
+
+	// 	lastMovementX = event.movementX;
+
+	// 	if ( reversals.length === shake_threshold && Date.now() - reversals[0] < 1000 )
+	// 		close();
+
+	// }
+
+	// document.addEventListener('mousemove', handler);
+
+	function dragHandler(e) {
+		let deltaX = e.clientX - start.x;
+
+		if ( deltaX * lastMovementX < 0 )
+			reversals.push(Date.now());
+
+		if ( reversals.length > shake_threshold ) reversals.shift();
+
+		lastMovementX = deltaX;
+
+		if ( reversals.length === shake_threshold && Date.now() - reversals[0] < 1000 )
+			close();
+
+	}
+
+	var start = null;
+
+	document.addEventListener('dragover', e => {
+	    start = {x: e.clientX, y: e.clientY}
+	}, {once: true});
+
+	document.addEventListener('dragover', dragHandler);
 }
