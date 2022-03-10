f33219964f9f68cee1644b2b3679eb4fa8735ee7
ssborbis <ssborbis@gmail.com>
2022-03-09 22:40:18 -0600

    quickmenu.css -> tilemenu.css

--- src/tilemenu.js
+++ src/tilemenu.js
@@ -758,7 +758,7 @@ async function makeQuickMenu(options) {
 			if ( !_singleColumn && tile.node && tile.node.type === 'folder' && tile.dataset.type === 'folder' ) {
 				
 				if ( tile.node.icon )
-					tile.dataset.hasicon = 'true'; // removes pseudo element label set by content:attr(data-title) in quickmenu.css 
+					tile.dataset.hasicon = 'true'; // removes pseudo element label set by content:attr(data-title) in tilemenu.css 
 				else
 					tile.style.backgroundImage = 'url(' + browser.runtime.getURL('icons/transparent.gif') + ')';
 			}
