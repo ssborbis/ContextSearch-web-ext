b282cfd9f9eeb0814e7838ae5805bb760b7a0694
ssborbis <ssborbis@gmail.com>
2025-01-21 00:45:22 -0600

    update: include svg icons for default images

--- src/defaultEngines.js
+++ src/defaultEngines.js
@@ -2,7 +2,7 @@ var defaultEngines = [
 	{
 		"searchForm": "https://www.google.com/",
 		"template": "https://www.google.com/search?q={searchTerms}",
-		"icon_url": "http://google.com/favicon.ico",
+		"icon_url": "/defaultEnginesIcons/google.svg",
 		"title": "Google",
 		"icon_base64String": "",
 		"method": "GET",
@@ -13,7 +13,7 @@ var defaultEngines = [
 	{
 		"searchForm": "https://www.amazon.com/",
 		"template": "https://www.amazon.com/exec/obidos/external-search/?field-keywords={searchTerms}&ie={inputEncoding}&mode=blended",
-		"icon_url": "https://www.amazon.com/favicon.ico",
+		"icon_url": "/defaultEnginesIcons/amazon.svg",
 		"title": "Amazon.com",
 		"icon_base64String": "",
 		"method": "GET",
@@ -24,7 +24,7 @@ var defaultEngines = [
 	{
 		"searchForm": "https://www.facebook.com/",
 		"template": "https://www.facebook.com/search/top/?q={searchTerms}&opensearch=1",
-		"icon_url": "http://facebook.com/favicon.ico",
+		"icon_url": "/defaultEnginesIcons/facebook.svg",
 		"title": "Facebook",
 		"icon_base64String": "",
 		"method": "GET",
@@ -35,7 +35,7 @@ var defaultEngines = [
 	{
 		"searchForm": "http://www.youtube.com",
 		"template": "http://www.youtube.com/results?search_query={searchTerms}",
-		"icon_url": "http://youtube.com/favicon.ico",
+		"icon_url": "/defaultEnginesIcons/youtube.svg",
 		"title": "YouTube",
 		"icon_base64String": "",
 		"method": "GET",
@@ -46,7 +46,7 @@ var defaultEngines = [
 	{
 		"searchForm": "https://twitter.com/",
 		"template": "https://twitter.com/search?q={searchTerms}&source=desktop-search",
-		"icon_url": "http://twitter.com/favicon.ico",
+		"icon_url": "/defaultEnginesIcons/twitter.svg",
 		"title": "Twitter",
 		"icon_base64String": "",
 		"method": "GET",
@@ -57,7 +57,7 @@ var defaultEngines = [
 	{
 		"searchForm": "http://www.pinterest.com/search/pins/",
 		"template": "http://www.pinterest.com/search/pins/?q={searchTerms}",
-		"icon_url": "http://pinterest.com/favicon.ico",
+		"icon_url": "/defaultEnginesIcons/pinterest.svg",
 		"title": "Pinterest",
 		"icon_base64String": "",
 		"method": "GET",
@@ -68,7 +68,7 @@ var defaultEngines = [
 	{
 		"searchForm": "https://www.bing.com/",
 		"template": "https://www.bing.com/search?q={searchTerms}",
-		"icon_url": "http://bing.com/favicon.ico",
+		"icon_url": "/defaultEnginesIcons/bing.svg",
 		"title": "Bing",
 		"icon_base64String": "",
 		"method": "GET",
@@ -79,7 +79,7 @@ var defaultEngines = [
 	{
 		"searchForm": "https://duckduckgo.com/",
 		"template": "https://duckduckgo.com/?q={searchTerms}",
-		"icon_url": "http://duckduckgo.com/favicon.ico",
+		"icon_url": "/defaultEnginesIcons/duckduckgo.svg",
 		"title": "DuckDuckGo",
 		"icon_base64String": "",
 		"method": "GET",
@@ -90,7 +90,7 @@ var defaultEngines = [
 	{
 		"searchForm": "https://www.ebay.com/",
 		"template": "https://www.ebay.com/sch/i.html?_nkw={searchTerms}",
-		"icon_url": "http://ebay.com/favicon.ico",
+		"icon_url": "/defaultEnginesIcons/ebay.svg",
 		"title": "eBay",
 		"icon_base64String": "",
 		"method": "GET",
@@ -101,7 +101,7 @@ var defaultEngines = [
 	{
 		"searchForm": "https://en.wikipedia.org/",
 		"template": "https://en.wikipedia.org/wiki/Special:Search?search={searchTerms}",
-		"icon_url": "http://wikipedia.org/favicon.ico",
+		"icon_url": "/defaultEnginesIcons/wikipedia.svg",
 		"title": "Wikipedia (en)",
 		"icon_base64String": "",
 		"method": "GET",
@@ -112,7 +112,7 @@ var defaultEngines = [
 	{
 		"searchForm": "https://images.google.com/",
 		"template": "https://www.google.com/searchbyimage?site=search&image_url={searchTerms}",
-		"icon_url": "http://icons.iconarchive.com/icons/designbolts/3d-social/256/Google-plus-icon.png",
+		"icon_url": "/defaultEnginesIcons/google_reverse.svg",
 		"title": "Google Reverse Image Search",
 		"icon_base64String": "",
 		"method": "GET",
@@ -123,7 +123,7 @@ var defaultEngines = [
 	{
 		"searchForm": "https://www.google.com",
 		"template": "https://www.google.com/search?q=site%3A{selectdomain}+{searchTerms}",
-		"icon_url": "https://www.google.com/images/branding/product/ico/googleg_lodp.ico",
+		"icon_url": "/defaultEnginesIcons/google_site.svg",
 		"title": "Google Site Search",
 		"icon_base64String": "",
 		"method": "GET",
